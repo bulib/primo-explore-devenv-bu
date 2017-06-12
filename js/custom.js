5516a18070f9c0efb12fb738066d7039d7cea43e
Mike Ward <mrward@bu.edu>
2017-06-12 13:40:04 -0400

    removing proxy.js

--- js/custom.js
+++ js/custom.js
@@ -7,6 +7,13 @@
 */
 var app = angular.module('viewCustom', ['angularLoad']);
 
+angular.element(document).ready(function () {
+  if (window.location.href.indexOf('.ezproxy.bu.edu') > 0) {
+    var n = window.location.href.replace('.ezproxy.bu.edu', '');
+    window.location.assign(n);
+  }
+});
+
 (function (i, s, o, g, r, a, m) {
   i['GoogleAnalyticsObject'] = r;i[r] = i[r] || function () {
     (i[r].q = i[r].q || []).push(arguments);
@@ -34,44 +41,5 @@ app.component('prmLogoAfter', {
   bindings: { parentCtrl: '<' },
   controller: 'prmLogoAfterController',
   template: '<div class="product-logo product-logo-local" layout="row" layout-align="start center" layout-fill id="banner" tabindex="0" role="banner">\n  <a href="https://www.bu.edu/library/search">\n  <img class="logo-image" alt="{{::(\'nui.header.LogoAlt\' | translate)}}" ng-src="{{$ctrl.getIconLink()}}"/>\n  </a>\n  </div>'
-});
-
-function addProxy() {
-  app.controller('prmServiceLinksAfterController', [function () {
-    var vm = this;
-    var parentCtrl = vm.parentCtrl;
-    var baseUrls = window.appConfig['mapping-tables']['Institution Base URLs'];
-
-    vm.$onInit = function () {
-      var pnxRecord = vm.parentCtrl.item.pnx;
-      var proxy = '';
-
-      // find the proxy URL - N.B. - assumes MT is already Institution-specific (i.e. only one proxy_prefix line)
-      angular.forEach(baseUrls, function (value, key) {
-        if (value.source2 == 'proxy_prefix') {
-          proxy = value.target;
-        }
-      });
-
-      if (parentCtrl.item.context == "PC" && (typeof pnxRecord.addata.oa == "undefined" || pnxRecord.addata.oa[0] != "free_for_read")) {
-        // PC record AND (no addata/oa OR addata/oa does not contain "free_for_read") => proxy
-        angular.forEach(parentCtrl.recordLinks, function (value, key) {
-          value.linkURL = proxy + value.linkURL;
-        });
-      }
-    };
-  }]);
-
-  app.component('prmServiceLinksAfter', {
-    bindings: {
-      parentCtrl: '<'
-    },
-    controller: 'prmServiceLinksAfterController'
-  });
-}
-addProxy();
-app.component('prmFavoritesToolBarAfter', {
-  bindings: {},
-  templateUrl: 'signin.html'
 });
 })();
