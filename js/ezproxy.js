4fb136cd078fad9c99fda10d634fff9611174782
Mike Ward <mrward@bu.edu>
2017-11-16 09:49:42 -0500

    handling ezproxy and primo https

--- js/ezproxy.js
+++ js/ezproxy.js
@@ -1,6 +1,6 @@
 angular.element(document).ready(function () {
-  if (window.location.href.indexOf('.ezproxy.bu.edu') > 0 ) {
-      var n = window.location.href.replace('.ezproxy.bu.edu','');
+  if (window.location.href.indexOf('buprimo-hosted-exlibrisgroup-com.ezproxy.bu.edu') > 0 ) {
+      var n = window.location.href.replace('buprimo-hosted-exlibrisgroup-com.ezproxy.bu.edu','buprimo.hosted.exlibrisgroup.com');
       window.location.assign(n);
   }
 });
