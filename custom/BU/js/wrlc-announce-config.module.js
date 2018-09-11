b853847e21d78e7b47b8b86a60a1b636fd81d00d
Aidan Sawyer <aidans@bu.edu>
2018-09-11 17:28:44 -0400

    banner: (-) customize wrlc-announce code locally
    note: ideally temporary until we can switch over to using their npm package via: https://github.com/wrlc-primo-dev/primo-explore-wrlc-announce/pull/8

---
+++ custom/BU/js/wrlc-announce-config.module.js
@@ -0,0 +1,31 @@
+//load app 'viewCustom' as a module with [] dependencies
+var app = angular.module('viewCustom',['angularLoad', 'wrlcAnnounce']);
+
+// - helper code for announcement banner for [`primo-explore-wrlc-announce` package](https://www.npmjs.com/package/primo-explore-wrlc-announce) - //
+app.constant('announceConfig', {
+
+  // view/edit the values in the regular view by using the same 'id' (/feeds/list<ID>/1/public below) in the following: (docs.google.com/spreadsheets/d/<SHEET_ID>)
+  announceAPI: 'https://spreadsheets.google.com/feeds/list/1dhGFCdOYlEG-DxkNs5F94WnHEmEIyTllQKhhWWtmmIE/1/public/values?alt=json',
+
+  // specify which of the N 'entries' (rows) you want the info for [defaulted to 0]
+  apiEntryNumber: 0,
+
+  // get the main data object associated with your desired view
+  getData: function(response) {
+    return response.data.feed.entry[this.apiEntryNumber];
+  },
+
+  // obtain the specifically relevant parts of that data object
+  getShow: function(entryData) {
+    return entryData.gsx$showbanner.$t;
+  },
+  getMessage: function(entryData) {
+    return entryData.gsx$messagetext.$t;
+  },
+  getLink: function(entryData) {
+    return entryData.gsx$messagelink.$t;
+  },
+  getSeverity: function(entryData) {
+    return entryData.gsx$messageseverity.$t;
+  }
+});
