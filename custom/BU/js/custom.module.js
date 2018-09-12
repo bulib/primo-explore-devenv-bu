e141d80b39810a08b787cb52f5e9f6d7304113df
Aidan Sawyer <aidans@bu.edu>
2018-09-12 14:25:12 -0400

    unpaywall: set icon to external link

--- custom/BU/js/custom.module.js
+++ custom/BU/js/custom.module.js
@@ -6,5 +6,6 @@ var app = angular.module('viewCustom',['angularLoad', 'oadoi']);
 
 // unpaywall
 app.constant('oadoiOptions', {
-  "email":"aidans@bu.edu"
+  "email":"aidans@bu.edu",
+  "imagePath":"custom/BU/img/resRec_launch_blue_2x.png"
 });
