22d1368757a94f229852f0737a15798cc61e4115
atla5 <aidans@bu.edu>
2018-09-13 18:00:15 -0400

    unpaywall: resolve 'linktorsrc'

--- custom/BU/js/unpaywall.js
+++ custom/BU/js/unpaywall.js
@@ -8,24 +8,35 @@ angular.module('bu-unpaywall', [])
         <table>
           <tr><td><strong>doi</strong></td><td>{{$ctrl.doi}}</td></tr>
           <tr><td><strong>fulltext</strong></td><td>{{$ctrl.fulltext}}</td>
-          <tr><td><strong>linktorcsc</strong></td><td>{{$ctrl.linktorcsc}}</td></tr>
+          <tr><td><strong>linktorsrc</strong></td><td>{{$ctrl.linktorsrc}}</td></tr>
+          <tr><td><strong>best_oa_location</strong></td><td></td></tr>
         </table>
       </oa-access>`,
     controller:
-      function unpaywallController($scope, $element) {
+      function unpaywallController(oadoiHelper, $scope, $element) {
         var item = this.parentCtrl.item;
         try{
+          console.log(item.pnx);
+
           // set known conditional data
           this.fulltext = item.pnx.delivery.fulltext[0];
-          this.linktorcsc = item.pnx.links.linktorcsc;
+
+          // set linktorsrc
+          if(item.pnx.links.linktorsrc){
+            // e.g. get the link out of "$$Uhttps://doaj.org/article/bb99cd21e1144744a75b1502e4affaf1$$E..."
+            var str = item.pnx.links.linktorsrc[0];
+            this.linktorsrc = str.substring("$$U".length, str.lastIndexOf("$$E"));
+          }
 
           //get at the doi
           var addata = item.pnx.addata;
-          console.log(addata);
           this.doi = (addata.hasOwnProperty("doi"))? addata.doi[0] : null;
+
+
+          }
         }catch(e){
+          console.log("error caught in unpaywallController: " + e);
           this.item = null;
         }
-        console.log(item);
       }
   });
