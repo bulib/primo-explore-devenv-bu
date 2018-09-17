eb0d4d50d7ee97fe06ba05f47a7a1523a52f3e75
atla5 <aidans@bu.edu>
2018-09-17 12:19:23 -0400

    unpaywall: [bugfix] 'self' vs. 'this' conflation inside .get

--- custom/BU/js/unpaywall.js
+++ custom/BU/js/unpaywall.js
@@ -9,19 +9,16 @@ angular
           <tr><td><strong>doi</strong></td><td>{{$ctrl.doi}}</td></tr>
           <tr><td><strong>fulltext</strong></td><td>{{$ctrl.fulltext}}</td>
           <tr><td><strong>linktorsrc</strong></td><td>{{$ctrl.linktorsrc}}</td></tr>
-          <tr><td><strong>best_oa_location</strong></td><td>{{$ctrl.best_oa_location}}</td></tr>
+          <tr><td><strong>best_oa_link</strong></td><td>{{$ctrl.best_oa_link}}</td></tr>
         </table>
       </oa-access>`,
     controller:
       function unpaywallController($scope, $http) {
+        var self = this;
         var item = this.parentCtrl.item;
         try{
-          //console.log(item.pnx);
-
           // set known conditional data
           this.fulltext = item.pnx.delivery.fulltext[0];
-
-          // set linktorsrc
           if(item.pnx.links.linktorsrc){
             // get the link out of "$$Uhttps://doaj.org/article/bb99cd21e1144744a75b1502e4affaf1$$E..."
             var str = item.pnx.links.linktorsrc[0];
@@ -32,19 +29,12 @@ angular
           var addata = item.pnx.addata;
           if(addata.hasOwnProperty("doi")){
             this.doi = addata.doi[0];
-            var url="https://api.oadoi.org/v2/"+this.doi+"?email=aidans@bu.edu";
-
-            //$http.defaults.headers.common = { 'X-From-ExL-API-Gateway': undefined }
-            var best_oa_link = $http.get(url)
+            $http.get("https://api.oadoi.org/v2/"+this.doi+"?email=aidans@bu.edu")
               .then(function(response){
-                console.log(response.data.best_oa_location.url);
-                return response.data.best_oa_location.url;
+                self.best_oa_link = response.data.best_oa_location.url;
               }, function(error){
                 console.log(error);
-                return "_nope_";
-              }).$$state;
-            console.log(best_oa_link.value);
-            this.best_oa_location = best_oa_link;
+              });
           }
 
         }catch(e){
