3bbb393e8d49b5be2175a9fb5750133dd06e14b1
atla5 <aidans@bu.edu>
2018-09-26 14:52:28 -0400

    unpaywall: (-) google analytics with unworking ajax
    difficulty importing/requiring jquery...

--- custom/BU/js/custom.module.js
+++ custom/BU/js/custom.module.js
@@ -1,7 +1,7 @@
 //load app 'viewCustom' as a module with [] dependencies
 var app = angular.module('viewCustom', ['angularLoad', 'oadoiResults', 'oadoi', 'wrlcAnnounce']);
 
-// unpaywall
+// - unpaywall - //
 app.constant('oadoiOptions', {
   "email":"aidans@bu.edu",
   "imagePath":"custom/BU/img/resRec_launch_blue_2x.png",
@@ -9,3 +9,16 @@ app.constant('oadoiOptions', {
   "showVersionLabel":true,
   "debug":false
 });
+
+/*
+import * as $ from "jquery";
+$('oadoi-results ~ a').on('click', function() {
+  console.log("oadoi-results click event");
+  _gaq.push(['_trackEvent', 'Unpaywall Brief Result', 'Clicked']);
+});
+
+$('oadoi ~ a').on('click', function() {
+  console.log("oadoi-full click event");
+  _gaq.push(['_trackEvent', 'Unpaywall Full Result', 'Clicked']);
+});
+*/
