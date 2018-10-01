5d23ca0ab2c4d4579388d32b2c7e3a4b6f8ba0ff
atla5 <aidans@bu.edu>
2018-10-01 10:32:55 -0400

    unpaywall: color up icon; arrow -> ext link icon, npm audit

--- custom/BU/js/unpaywall.module.js
+++ custom/BU/js/unpaywall.module.js
@@ -4,13 +4,13 @@ angular.module('unpaywall', [])
     template: `
       <unpaywall ng-if="$ctrl.show">
         <div layout="flex" ng-if="$ctrl.best_oa_link" class="layout-row" style="margin-top: 5px;">
-          <prm-icon icon-type="svg" svg-icon-set="action" icon-definition="ic_lock_open_24px"></prm-icon>
+          <prm-icon icon-type="svg" svg-icon-set="action" icon-definition="ic_lock_open_24px" style="color: #f68212;"></prm-icon>
           <a class="arrow-link-button md-primoExplore-theme md-ink-ripple" style="margin-left: 3px; margin-top: 3px;"
              ng-click="$ctrl.trackLinkClick($ctrl.doi)" target="_blank" href="{{$ctrl.best_oa_link}}" rel="noreferrer">
              <strong>Open Access</strong> available via unpaywall
              <span ng-if="$ctrl.showVersionLabel && $ctrl.best_oa_version">&nbsp({{$ctrl.best_oa_version}} version)</span>
           </a>
-          <prm-icon link-arrow icon-type="svg" svg-icon-set="primo-ui" icon-definition="chevron-right"></prm-icon>
+          <prm-icon icon-type="svg" svg-icon-set="primo-ui" icon-definition="open-in-new" style="padding-left: 5px;"></prm-icon>
         </div>
         <div ng-if="$ctrl.debug" class="layout-row">
           <table>
