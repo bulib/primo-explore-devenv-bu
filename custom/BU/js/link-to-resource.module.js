703cf4ef5828d1440fd0519955bba79dcc3b90cb
atla5 <aidans@bu.edu>
2019-01-24 12:43:50 -0500

    WEB-59: successfully get 'more links' events registered

--- custom/BU/js/link-to-resource.module.js
+++ custom/BU/js/link-to-resource.module.js
@@ -1,11 +1,17 @@
-debug = true;
+app.constant('l2rHelper', {
+  debug: true,
+  sendGAEvent: false,
+  logL2rMessage: function(message){
+    if(this.debug){
+      console.log("link2resource) " + message);
+    }
+  },
+  logLinkToResource: function(fullViewOrMore, urlClicked){
+    this.logL2rMessage(`calling gaEventLogger with category:'link-to-resource', action:'${fullViewOrMore}', label:'${urlClicked}'`);
 
-app.service('linkToResourceHelper', function(gaEventLogger){
-  this.logLinkToResource = function(fullViewOrMore, urlClicked){
-    if(debug){
-      console.log(`link-to-resource: calling gaEventLogger with category:'link-to-resource', action:'${fullViewOrMore}', label:'${urlClicked}'`);
-    }else{
+    if(this.sendGAEvent){
       gaEventLogger.logEvent("link-to-resource", fullViewOrMore, urlClicked);
+      console.log("'link-to-resource' event sent to GA");
     }
   }
 });
@@ -13,65 +19,24 @@ app.service('linkToResourceHelper', function(gaEventLogger){
 angular.module('l2rMoreLinks', [])
   .component('prmServiceLinksAfter', {
     bindings: { parentCtrl: '<'},
-    controller: function l2rMoreLinks(linkToResourceHelper){
-      let links = this.parentCtrl.getLinks();
-      if(debug){ console.log(links); }
+    controller: function l2rMoreLinks(l2rHelper, $timeout){
+      $timeout(function(){ // wait for the links to be loaded into the page
 
-      let i, link, url, linkText;
-      for(i=0; i<links.length; i++){
-        link = links[i];
-        url = link.linkURL;
-        linkText = link.displayLabel.replace("$$E","");
+        // find the associated 'More Links' using the querySelectorAll
+        l2rHelper.logL2rMessage("using the querySelectorAll to grab l2rMoreLinks...");
+        let l2rMoreLinks = document.querySelectorAll("prm-service-links > div > div > a.arrow-link");
+        l2rHelper.logL2rMessage("obtained " + l2rMoreLinks.length + " anchor tags in 'More Links'.");
 
-        if(debug){
-          console.log("l2rMoreLinks: logging link: '" + linkText + "' to url: '" + url + "'");
-        }else{
-          linkToResourceHelper.logLinkToResource("more-links",url);
+        // add the logging event to each of these anchor links
+        for(let i=0; i<l2rMoreLinks.length; i++){
+          let anchorLinkElem = l2rMoreLinks[i];
+          let url = anchorLinkElem.getAttribute("href");
+          anchorLinkElem.addEventListener("click", function(event){
+            event.preventDefault();
+            l2rHelper.logLinkToResource("more-links", url);
+            window.open(url, '_blank');
+          });
         }
-      }
+      }, 2500)  // code above executes 2.5 seconds after the component first loads
     }
   });
-
-  angular.module('l2rResultsList', [])
-    .component('prmServiceHeaderAfter', {
-      bindings: { parentCtrl: '<'},
-      controller: function l2rFindONline(linkToResourceHelper){
-        let links = this.parentCtrl.displayedAvailability();
-        if(debug){ console.log(links); }
-
-        let i, link, url, linkText;
-        for(i=0; i<links.length; i++){
-          link = links[i];
-          url = link.linkURL;
-          linkText = link.displayLabel.replace("$$E","");
-
-          if(debug){
-            console.log("l2rResultsList: logging link: '" + linkText + "' to url: '" + url + "'");
-          }else{
-            linkToResourceHelper.logLinkToResource("more-links",url);
-          }
-        }
-      }
-    });
-
-  angular.module('l2rFindOnline', [])
-    .component('prmServiceHeaderAfter', {
-      bindings: { parentCtrl: '<'},
-      controller: function l2rFindONline(linkToResourceHelper){
-        let links = this.parentCtrl.getLinks();
-        if(debug){ console.log(links); }
-
-        let i, link, url, linkText;
-        for(i=0; i<links.length; i++){
-          link = links[i];
-          url = link.linkURL;
-          linkText = link.displayLabel.replace("$$E","");
-
-          if(debug){
-            console.log("l2rFindOnline: logging link: '" + linkText + "' to url: '" + url + "'");
-          }else{
-            linkToResourceHelper.logLinkToResource("more-links",url);
-          }
-        }
-      }
-    });
