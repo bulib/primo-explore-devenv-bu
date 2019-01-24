b0aa16d888fb90770c354b05da006fc018a01646
atla5 <aidans@bu.edu>
2019-01-24 16:48:06 -0500

    WEB-59: put protection around undefined.length

--- custom/BU/js/outbound-link-logger.module.js
+++ custom/BU/js/outbound-link-logger.module.js
@@ -28,48 +28,64 @@ app.constant('outboundLinksHelper', {
 angular.module('outboundLinksLogger', [])
   .component('prmServiceLinksAfter', {
     controller: function outboundLinksController(outboundLinksHelper, gaEventLogger, $timeout){
+      outboundLinksHelper.logOutboundLinkMessage("component loaded.");
       $timeout(function(){
         // find the associated 'More Links' using the querySelectorAll
         outboundLinksHelper.logOutboundLinkMessage("using the querySelectorAll to grab outbound links in 'More Links'...");
         let outboundLinks = document.querySelectorAll("prm-service-links > div > div > a.arrow-link");
-        outboundLinksHelper.logOutboundLinkMessage("adding eventListeners to " + outboundLinks.length + " 'outbound-links' anchor tags...");
 
-        // add the logging event to each of these anchor links
-        for(let i=0; i<outboundLinks.length; i++){
-          let anchorLinkElem = outboundLinks[i];
-          let url = anchorLinkElem.getAttribute("href");
-          let linkText = anchorLinkElem.querySelector("span").innerHTML;
-          anchorLinkElem.addEventListener("click", function(event){
-            event.preventDefault();
-            outboundLinksHelper.logOutboundLinkEvent(gaEventLogger, "outbound-link", linkText, url);
-            window.open(url, '_blank');
-          });
+        // if links are found, add eventListeners to them
+        if(outboundLinks && outboundLinks.length > 0){
+          outboundLinksHelper.logOutboundLinkMessage("adding eventListeners to " + outboundLinks.length + " 'outbound-links' anchor tag/s...");
+
+          // add the logging event to each of these anchor links
+          for(let i=0; i<outboundLinks.length; i++){
+            let anchorLinkElem = outboundLinks[i];
+            let url = anchorLinkElem.getAttribute("href");
+            let linkText = anchorLinkElem.querySelector("span").innerHTML;
+            anchorLinkElem.addEventListener("click", function(event){
+              event.preventDefault();
+              outboundLinksHelper.logOutboundLinkEvent(gaEventLogger, "outbound-link", linkText, url);
+              window.open(url, '_blank');
+            });
+          }
+
+        // if no links are found..
+        }else{
+          outboundLinksHelper.logOutboundLinkMessage("no 'outbound-links' found in 'More Links'.")
         }
       }, 1500);  // code above executes 1.5 seconds after the component first loads
 
       $timeout(function(){
         // find the 'Find Online' and 'View Online' sections
-        outboundLinksHelper.logOutboundLinkMessage("using the querySelectorAll to grab outbound links from 'Find Online' and 'View Online'...");
+        outboundLinksHelper.logOutboundLinkMessage("using the querySelectorAll to grab outbound links from 'Find/View Online'...");
         let linksToResource = document.querySelectorAll("prm-view-online > div > a");
-        outboundLinksHelper.logOutboundLinkMessage("adding eventListeners to " + linksToResource.length + " 'link-to-resource' anchor tags...");
-        for(let i=0; i<linksToResource.length; i++){
-          let anchorLinkElem = linksToResource[i];
 
-          // determine the source information (e.g. libguides, openBU) from the primo 'docid' value
-          let source = outboundLinksHelper.getHrefArgFromSearch(window.location.search, "docid=");
-          // outboundLinksHelper.logOutboundLinkMessage("source: " + source);
+        if(linksToResource && linksToResource.length > 0){
+          outboundLinksHelper.logOutboundLinkMessage("adding eventListeners to " + linksToResource.length + " 'link-to-resource' anchor tag/s...");
 
-          // get the url (and use the referring if it's an easyproxy link)
-          let url = anchorLinkElem.getAttribute("href");
-          url = outboundLinksHelper.getHrefArgFromSearch(url, "url=", url);
-          // outboundLinksHelper.logOutboundLinkMessage("url: " + url);
+          for(let i=0; i<linksToResource.length; i++){
+            let anchorLinkElem = linksToResource[i];
 
-          anchorLinkElem.addEventListener("click", function(event){
-            event.preventDefault();
-            outboundLinksHelper.logOutboundLinkEvent(gaEventLogger, "link-to-resource",source, url);
-            window.open(url, '_blank');
-          });
-        }
+            // determine the source information (e.g. libguides, openBU) from the primo 'docid' value
+            let source = outboundLinksHelper.getHrefArgFromSearch(window.location.search, "docid=");
+
+            // get the url (and use the referring if it's an easyproxy link)
+            let url = anchorLinkElem.getAttribute("href");
+            url = outboundLinksHelper.getHrefArgFromSearch(url, "url=", url);
+
+            // add the eventListener to the anchor tag
+            anchorLinkElem.addEventListener("click", function(event){
+              event.preventDefault();
+              outboundLinksHelper.logOutboundLinkEvent(gaEventLogger, "link-to-resource",source, url);
+              window.open(url, '_blank');
+            });
+          }
+
+          // if no links are found..
+          }else{
+            outboundLinksHelper.logOutboundLinkMessage("no 'link-to-resource' found in 'Find/View Online'.")
+          }
       }, 2500); // code above executes 2.5 seconds after the component first loads
     }
   });
