58befe9b32a5226ef1cab476ea39668eaad52b6c
atla5 <aidans@bu.edu>
2019-02-06 13:51:54 -0500

    WEB-33: remove custom js, wrlc-announce css

--- custom/ISL/js/ezproxy.js
+++
@@ -1,6 +0,0 @@
-angular.element(document).ready(function () {
-  if (window.location.href.indexOf('buprimo-hosted-exlibrisgroup-com.ezproxy.bu.edu') > 0 ) {
-      var n = window.location.href.replace('buprimo-hosted-exlibrisgroup-com.ezproxy.bu.edu','buprimo.hosted.exlibrisgroup.com');
-      window.location.assign(n);
-  }
-});
