e39797ecf40eeb2e8eaeab596e870b1ba8034b53
atla5 <aidans@bu.edu>
2019-02-08 12:59:51 -0500

    WEB-72: remove draggable code, add 'open in new window'
    refactor 'dialogTemplate'

--- custom/BU/js/help-menu.module.js
+++ custom/BU/js/help-menu.module.js
@@ -1,6 +1,7 @@
 app.constant('helpMenuHelper', {
   debug: true,
   sendGAEvent: false,
+  helpMenuWidth: 450,
   logMessage: function(message){
     if(this.debug){
       console.log("helpMenu) " + message);
@@ -14,46 +15,36 @@ app.constant('helpMenuHelper', {
       gaEventLogger.logEvent(category, action, label, this.debug);
     }
   },
-  dialogTemplate: `
-    <md-dialog id="search-help-dialog" aria-label="Search Help Menu Dialog" ng-mouseleave=dragMouseUp()
-               style="width: 450px;">
-      <form>
-        <md-toolbar id="search-help-dialog-header" style="cursor: move;" 
-                    ng-mousedown=dragMouseDown(ev) ng-mousemove=elementDrag(ev)>
-          <div class="md-toolbar-tools">
-            <a ng-if="entry" class="md-icon-button md-button md-primoExplore-theme md-ink-ripple" ng-click="back()">
-              <prm-icon aria-label="Close dialog" icon-type="svg" svg-icon-set="navigation" icon-definition="ic_arrow_back_24px"></prm-icon>
-            </a>
-            <h2><span ng-hide="entry">Search Help</span><span ng-hide="!entry">{{entry.title}}</span></h2>
-            <span flex></span>
-            <a class="md-icon-button md-button md-primoExplore-theme md-ink-ripple" ng-click="hide()">
-              <prm-icon aria-label="Close dialog" icon-type="svg" svg-icon-set="navigation" icon-definition="ic_close_24px"></prm-icon>
-            </a>
-          </div>
-        </md-toolbar>
-        <md-dialog-content>
-          <div class="md-dialog-content">
-            <div ng-if="entry" id="search-help-dialog-content">
-              <p ng-if="!entry.template"><em>{{entry.description}}</em></p>
-              <div ng-bind-html="entry.template"></div>
-            </div>
-            <ul ng-hide="entry" style="list-style: none; width: 100%; padding-left: 0px;">
-              <hr />
-              <li ng-repeat="item in helpContentList" class="row">
-                <a ng-if="item.id" ng-click="openItem(item.id)">
-                  <prm-icon svg-icon-set="{{item.icon.group}}" icon-definition="ic_{{item.icon.code}}_24px"
-                            icon-type="svg" style="padding-right: 10px;"></prm-icon>
-                  <span>{{item.title}}</span>
-                </a>
-                <hr ng-if="!item.id"/>
-              </li>
-              <hr />
-            </ul>
-          </div>
-        </md-dialog-content>
-      </form>
-    </md-dialog>
-  `,
+  helpContentBody:`
+    <div class="md-dialog-content">
+      <div ng-if="entry" id="search-help-dialog-content">
+        <p ng-if="!entry.template"><em>{{entry.description}}</em></p>
+        <div ng-bind-html="entry.template"></div>
+      </div>
+      <ul ng-hide="entry" style="list-style: none; width: 100%; padding-left: 0px;">
+        <hr />
+        <li ng-repeat="item in helpContentList" class="row">
+          <a ng-if="item.id" ng-click="openItem(item.id)">
+            <prm-icon svg-icon-set="{{item.icon.group}}" icon-definition="ic_{{item.icon.code}}_24px"
+                      icon-type="svg" style="padding-right: 10px;"></prm-icon>
+            <span>{{item.title}}</span>
+          </a>
+          <hr ng-if="!item.id"/>
+        </li>
+        <hr />
+      </ul>
+    </div>`,
+  helpContentHeader: `
+    <div class="md-toolbar-tools">
+      <a ng-if="entry" class="md-icon-button md-button md-primoExplore-theme md-ink-ripple" ng-click="back()">
+        <prm-icon aria-label="Close dialog" icon-type="svg" svg-icon-set="navigation" icon-definition="ic_arrow_back_24px"></prm-icon>
+      </a>
+      <h2><span ng-hide="entry">Search Help</span><span ng-hide="!entry">{{entry.title}}</span></h2>
+      <span flex></span>
+      <a class="md-icon-button md-button md-primoExplore-theme md-ink-ripple" ng-click="hide()">
+        <prm-icon aria-label="Close dialog" icon-type="svg" svg-icon-set="navigation" icon-definition="ic_close_24px"></prm-icon>
+      </a>
+    </div>`,
   openDialogBox: function(){
     const dialog = new MDCDialog(document.querySelector('.mdc-dialog'));
     dialog.listen('MDCDialog:opened', () => {
@@ -81,26 +72,38 @@ angular.module('helpMenuTopbar', ['ngMaterial'])
         helpMenuHelper.logHelpEvent(gaEventLogger, "opened", window.location.pathname);
         $mdDialog.show({
           controller: helpMenuDialogController,
-          template: helpMenuHelper.dialogTemplate,
+          template: `
+            <md-dialog id="search-help-dialog" aria-label="Search Help Menu Dialog" ng-mouseleave=dragMouseUp()
+                      style="width: ${helpMenuHelper.helpMenuWidth}px;">
+              <form>
+              <md-toolbar ng-mousedown=dragMouseDown(ev) ng-mousemove=elementDrag(ev)>
+                <div id="search-help-dialog-header" class="md-toolbar-tools">
+                  ${helpMenuHelper.helpContentHeader}
+                </div>
+              </md-toolbar>
+              <md-dialog-content id="help-dialog-content">
+                ${helpMenuHelper.helpContentBody}
+              </md-dialog-content>
+                <md-dialog-actions layout="row">
+                  <md-button ng-click="openHelpInNewWindow()">Open in New Window</md-button>
+                </md-dialog-actions>
+              </form>
+            </md-dialog>`,
           parent: angular.element(document.body),
           targetEvent: ev,
-          hasBackdrop: false,
-          clickOutsideToClose:false,
+          hasBackdrop: true,
+          clickOutsideToClose:true,
           fullscreen: false,
           focusOnOpen: true
         });
-        
+        document.querySelector("#search-help-dialog-header").innerHTML = helpMenuHelper.helpContentHeader;
+        document.querySelector("#md-dialog-content").innerHTML = helpMenuHelper.helpContentBody;
       };
 
       function helpMenuDialogController(helpMenuContent, $scope, $mdDialog) {
 
         // data
         $scope.helpContentList = helpMenuContent.list_of_elements;
-        $scope.selected = false;
-        $scope.pos1 = 0;
-        $scope.pos2 = 0;
-        $scope.pos3 = 0;
-        $scope.pos4 = 0;
         
         // modal navigation
         $scope.hide = function() { $mdDialog.hide(); };
@@ -112,35 +115,9 @@ angular.module('helpMenuTopbar', ['ngMaterial'])
           $scope.entry = helpMenuContent.get_entry_by_id(id);
           helpMenuHelper.logHelpEvent(gaEventLogger, "selected", id);
         };
-
-        // dragging modal
-        $scope.dragMouseDown = function(ev){
-          ev = ev || window.event;
-          helpMenuHelper.logMessage("dragMouseDown");
-          $scope.selected = true; 
-          $scope.pos3 = ev.clientX;
-          $scope.pos4 = ev.clientY;
-        }
-        $scope.elementDrag = function(e){
-          if(!$scope.selected){ return; }
-          console.log("elementDrag");
-          e = e || window.event;
-          // e.preventDefault();
-          // calculate the new cursor position:
-          $scope.pos1 = $scope.pos3 - e.clientX;
-          $scope.pos2 = $scope.pos4 - e.clientY;
-          $scope.pos3 = e.clientX;
-          $scope.pos4 = e.clientY;
-          // set the element's new position:
-          let elmntHeader = document.getElementById("search-help-dialog-header");
-          let elmnt = document.getElementById("search-help-dialog");
-          elmnt.style.top = (elmntHeader.offsetTop - $scope.pos2) + "px";
-          elmnt.style.left = (elmntHeader.offsetLeft - $scope.pos1) + "px";
-          elmnt.onmouseup = $scope.dragMouseUp;
-        };
-        $scope.dragMouseUp = function(ev){
-          $scope.selected = false;
-          helpMenuHelper.logMessage("dragMouseUp");
+        $scope.openHelpInNewWindow = function(){
+          let params=`width=500,height=800,resizable=0,location=0,menubar=0,scrollbar=1`;
+          open('/primo-explore/search?vid=BU', 'BULibraries Help Menu', params);
         }
       }
     }
