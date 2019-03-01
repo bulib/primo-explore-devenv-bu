c861ce47c5b29a06f0ed85d89dfb14fdb5cc364e
atla5 <aidans@bu.edu>
2019-03-01 12:47:42 -0500

    WEB-80: move google analytics code into main.js
    i'd have loved to keep it more separated than that, but it's not worth it to keep pursuing at this point,

--- custom/BU/js/main.js
+++ custom/BU/js/main.js
@@ -1,40 +1,57 @@
 // import helpers
-import {gaRunList, gaEventLogger} from './ga';
 import './load-helpers';
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
 let INCLUDE_GOOGLE_ANALYTICS = true;
 if(INCLUDE_GOOGLE_ANALYTICS){
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-    
   ga('create', 'UA-5204430-3', 'auto');
   ga('send', 'pageview');
 }
 
 // import npm packages
-import 'primo-explore-report-problem';
 import 'primo-explore-unpaywall';
 
 // import other custom modules
 import './outbound-link-logger.module';
 import './wrlc-announce.module';
 
-
 // create the main primo-explore module and load in its local and npm-imported dependencies 
 angular.module('viewCustom', 
-    ['angularLoad', 'bulibUnpaywall', 'outboundLinksLogger', 'reportProblem', 'wrlcAnnounce']
+    ['angularLoad', 'bulibUnpaywall', 'outboundLinksLogger', 'wrlcAnnounce']
   )
 
-  // google analytics
-  .run(gaRunList)
-  .service('gaEventLogger', gaEventLogger)
+  // google analytics 
+  .run(['$rootScope', '$location', '$window', function($rootScope, $location, $window){
+    $rootScope.$on('$locationChangeSuccess', function(event){
+      if(INCLUDE_GOOGLE_ANALYTICS){
+        $window.ga('send', 'pageview', {location: $location.url()});
+      }
+    });
+  }])
+  .service('gaEventLogger', function(){
+    this.logAboutEvent = function(message, debug=false){
+      if(debug){ console.log("gaEventLogger) " + message); }
+    }
+    this.logEvent = function(category="", action="", label="", debug=false){
+      var eventMessage = "cat:'"+category + "' act:'"+action + "' label:'"+label+"'";
+      if(window.ga){
+        window.ga('send', 'event', category, action, label);
+        this.logAboutEvent("new '" + category + "' event sent to Google Analytics", debug);
+      }else{
+        this.logAboutEvent("window.ga not found! cannot log message: '" + eventMessage + "'.", debug);
+      }
+    }
+  })
 
   // configure bulibUnpaywall
   .constant('unpaywallConfig', {
     "email":"aidans@bu.edu",
-    "logToConsole":true
+    "logToConsole":true,
+    "publishEvents":true
   })
 
   // configure wrlc announce
@@ -42,6 +59,7 @@ angular.module('viewCustom',
 
     // view/edit the values in this spreadsheet by using the same 'id' (/feeds/list/<ID>/1/public) in the following: (docs.google.com/spreadsheets/d/<SHEET_ID>)
     announceAPI: 'https://spreadsheets.google.com/feeds/list/1ElW0CUOV3LvcHuYxK2BZfFjo65a-XDrlNJtnrelA6tM/1/public/values?alt=json',
+
     // specify which of the N 'entries' (zero-based row id [not including headers]) you want the info for [defaulted to 0]
     apiEntryNumber: 0,
   
