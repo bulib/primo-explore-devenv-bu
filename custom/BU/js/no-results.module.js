f9b6dc6eef7e9e44f5021be3856b0a5b1ce4eb01
atla5 <aidans@bu.edu>
2019-09-26 14:29:02 -0400

    WEB-201: add 'ng-if's for checks, actions, responding to query

--- custom/BU/js/no-results.module.js
+++ custom/BU/js/no-results.module.js
@@ -1,29 +1,25 @@
 const quick_actions = `
   <md-card>
     <md-card-title>
+      <md-card-title-text><span translate="" class="md-headline ng-scope">Quick Actions</span></md-card-title-text>
     </md-card-title>
     <md-card-content>
-      <h3>Quick Actions</h3>
-      <div class="deck">
-        <bulib-card class="full-width" title="Search without Quotes" icon="format_quote"
-          description="Run the same query without the quotes"></bulib-card>
-        <bulib-card class="full-width" title="Expand Search" icon="zoom_out_map"
-          description="Try searching over a larger set of results"></bulib-card>
-        <bulib-card class="full-width" title="Ask for Help" icon="people"
-          description="Get assistance with your search from a real librarian!"><bulib-card>
-      </div>
-      <ul class="no-bullet">
-        <li>
-          <md-button>Search without Quotes</md-button>
-          <span>Rerun the same query, but <strong>remove the quotes</strong></span>
+      <ul class="deck no-bullet">
+        <li id="action-quotes" ng-if="$ctrl.showQuotes">
+          <bulib-card small title="Search without Quotes" icon="format_quote" debug description="Run the same query without the quotes"
+            action="console.log('searchWithoutQuotes()')"></bulib-card>
         </li>
-        <li>
-          <md-button>Expand Search</md-button>
-          Try searching over a <strong>larger set</strong> of results
+        <li id="action-scope" ng-if="$ctrl.showScopes">
+          <bulib-card small title="Expand Search" icon="zoom_out_map" description="Try searching over a larger set of results"
+            action="console.log('searchWithoutQuotes()')"></bulib-card> 
         </li>
-        <li>
-          <md-button>Ask for Help</md-button>
-          Get assistance with your search from a <strong>real librarian</strong>!
+        <li id="action-filter" ng-if="$ctrl.showFilter">
+          <bulib-card small title="Remove Filters" icon="label_off" description="Remove active filters "
+            action="console.log('searchWithoutFilters()')"></bulib-card> 
+        </li>
+        <li id="action-chat">
+          <bulib-card small title="Ask for Help" icon="people" description="Get assistance with your search from a real librarian!"
+            action="console.log('openChat()')"><bulib-card>
         </li>
       </ul>
     </md-card-content>
@@ -36,27 +32,29 @@ const troubleshooting_checklist = `
     </md-card-title>
     <md-card-content> 
       <ul class="no-bullet">
-        <li>
+        <li id="check-quotes">
+          <label>
+            <input ng-hide="!$ctrl.showQuotes" type="checkbox" value="search-scope">
+            <input ng-hide="$ctrl.showQuotes"  type="checkbox" value="search-scope" checked>
+            Check for searches in <strong>quotation marks</strong>
+          </label>
+        </li>
+        <li id="check-spelling">
           <label>
             <input type="checkbox" name="check" value="spelling">
             Check for <strong>misspelled words or typos</strong>.
           </label>
         </li>
-        <li>
-          <label>
-            <input type="checkbox" name="check" value="quotations" checked>
-            Check for searches in <strong>quotation marks</strong>
-          </label>
-        </li>
-        <li>
+        <li id="check-query">
           <label>
             <input type="checkbox" value="adjust-query">
             <strong>Adjust your query</strong> to contain fewer or more general/common keywords
           </label>
         </li>
-        <li>
+        <li id="check-scope">
           <label>
-            <input type="checkbox" value="search-scope">
+            <input ng-hide="!$ctrl.showScopes" type="checkbox" value="search-scope">
+            <input ng-hide="$ctrl.showScopes"  type="checkbox" value="search-scope" checked>
             <strong>Try another <a href="/primo-explore/search?vid=BU">search scope</a></strong> 
             that's better-suited to your query
           </label>
@@ -79,11 +77,23 @@ angular.module('noResults', [])
     var vm = this;
     vm.getSearchTerm = getSearchTerm;
     vm.pciSetting = vm.parentCtrl.searchStateService.searchObject.pcAvailability || '';
-    function getSearchTerm() {
-      return vm.parentCtrl.term;
+    function getSearchTerm() { return vm.parentCtrl.term; }
+    function getCurrentPathWithArgs(){ return window.location.pathname+window.location.search; }
+
+    // check various conditions to determine which quick actions to show
+    this.showQuotes = this.getSearchTerm().includes('"'); 
+    this.showFilter = window.location.search.includes("mfacet=");
+    this.showScopes = !window.location.search.includes("search_scope=pci_all");
+
+    // action helpers
+    function searchWithoutQuotes(){
+      let newLink = this.getCurrentPathWithArgs().replace('/"/g','');
+      console.log("'" + this.getCurrentPathWithArgs() + "' -> '" + newLink + "'")
+      return newLink;
     }
-    function checkTypos(searchTerm){ return true; }
-    function checkQuotes(searchTerm){ return true; }
+    function searchWithoutFilters(){
+      let newLink = this.getCurrentPathWithArgs.replace('/')
+    }
   }])
 
   // Update links in template line below to direct to your Primo server and WorldCat, etc.
@@ -94,6 +104,7 @@ angular.module('noResults', [])
       <style type="text/css">
         /* hide the first boilerplate */
         prm-no-search-result > md-card { display: none; }
+        bulib-card a { text-decoration: none !important; }
       </style>
       <md-card>
         <md-card-title>
