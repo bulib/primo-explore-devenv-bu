3e83db2e6fa0da688ae61f2af4bc644273b29534
atla5 <aidans@bu.edu>
2020-01-13 17:18:32 -0500

    WEB-188: consume <bulib-announce> from bulib-wc
    simplify/extract/augment existing 'wrlc-announce'
    
    - update 'bulib-wc' version
    - move remaining 'announce.css' into BU.css
    - drastically simplify prmSearchBarAfter component template

--- custom/BU/js/announce-banner.module.js
+++ custom/BU/js/announce-banner.module.js
@@ -28,38 +28,16 @@ angular.module('announceBanner', ['ngAnimate'])
         self.message = config.getMessage(data);
         self.link = config.getLink(data);
         self.severity = (config.getSeverity)? config.getSeverity(data) : 'info';
+        self.cta_text = data.gsx$ctatext.$t;
       });
-
-      //respond to user exing out of the banner
-      self.wrDismiss = function() {
-        self.dismissed = true;
-      }
     }
   ])
   .component('prmSearchBarAfter', {
     template: `
-      <wrlc-announce ng-hide="$ctrl.dismissed" ng-if="$ctrl.show">
-        <style ng-if="$ctrl.show && !$ctrl.dismissed">
-          /* add padding under the search bar to make room for banner */ 
-          .__sm prm-search-bar { padding-bottom: 60px; }
-          prm-search-bar { padding-bottom: 68.5px; }
-          .__xs prm-search-bar { padding-bottom: 60px; }
-        </style>
-        <div class="announce-banner" class="layout-align-center-center layout-row flex {{$ctrl.severity}}">
-          <prm-icon ng-if="$ctrl.severity=='info'" icon-type="svg" svg-icon-set="action" icon-definition="ic_info_24px"></prm-icon>
-          <prm-icon ng-if="$ctrl.severity=='alert'" icon-type="svg" svg-icon-set="action" icon-definition="ic_announcement_24px"></prm-icon>
-          <prm-icon ng-if="$ctrl.severity=='warn'" icon-type="svg" svg-icon-set="action" icon-definition="ic_report_problem_24px"></prm-icon>
-          <prm-icon ng-if="$ctrl.severity=='success'" icon-type="svg" svg-icon-set="action" icon-definition="ic_check_circle_24px"></prm-icon>
-          <span ng-if="$ctrl.link" class="message"><a target="_blank" href="{{$ctrl.link}}" class="txtv" tabindex="0">{{ $ctrl.message }}</a></span>
-          <span ng-if="!$ctrl.link" ng-bind-html=$ctrl.message id="message"></span>
-    
-          <button id="dismiss-announcement" area-label="dismiss announcement" type="button" ng-click="$ctrl.wrDismiss()"
-                  class="dismiss-alert-button md-button md-primoExplore-theme button-with-icon">
-            <prm-icon icon-type="svg" svg-icon-set="navigation" icon-definition="ic_close_24px" class="material-icons gray"></prm-icon>
-            <span translate="nui.message.dismiss" hide-xs="" class="hide-xs txtv">DISMISS</span>
-          </button>
-        </div>
-      </wrlc-announce>
+      <bulib-announce code="primo-explore"
+        message="{{$ctrl.message}}" severity="{{$ctrl.severity}}" 
+        cta_text="{{$ctrl.cta_text}}" cta_url="{{$ctrl.link}}">
+      </bulib-announce>
     `,
     controller: 'announceController'
   });
