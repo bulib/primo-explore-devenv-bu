533df2de710b3fadf589f8386d2054137fdcd61f
atla5 <aidans@bu.edu>
2020-01-15 14:55:29 -0500

    WEB-188: [docs] remove references to wrlc-announce, debug

--- custom/BU/js/announce-banner.module.js
+++ custom/BU/js/announce-banner.module.js
@@ -1,13 +1,8 @@
-/* - Add conditional banner to all primo pages for time-sensitive messages - 
- *
- * NOTE: this is based entirely off of [`primo-explore-wrlc-announce](https://github.com/wrlc-primo-dev) and their
- *  associated [npm package](https://www.npmjs.com/package/primo-explore-wrlc-announce).
- *
- * All credit goes to them, and this file is only here until BU-Lib specific functionality can be added to the
- *   existing repo (e.g. until [our PR](https://github.com/wrlc-primo-dev/primo-explore-wrlc-announce/pull/8)
- *   is merged and the npm package is versioned to where we can use that instead).
+/** 
+ * Add a conditional banner to all primo pages for time-sensitive messages 
+ *   - information obtained from Google Sheets API 
+ *   - frontend display consumes the <bulib-announce> web component from `bulib-wc`
  */
-
 angular.module('announceBanner', ['ngAnimate'])
   .controller('announceController', ['announceConfig', '$http', 
     function(announceConfig, $http){
@@ -34,7 +29,7 @@ angular.module('announceBanner', ['ngAnimate'])
   ])
   .component('prmSearchBarAfter', {
     template: `
-      <bulib-announce code="primo-explore"
+      <bulib-announce code="primo-explore" debug
         message="{{$ctrl.message}}" severity="{{$ctrl.severity}}" 
         cta_text="{{$ctrl.cta_text}}" cta_url="{{$ctrl.link}}">
       </bulib-announce>
