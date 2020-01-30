ff4f362c358de8bb18e20a721411cb7f26c182b9
atla5 <aidans@bu.edu>
2020-01-30 13:39:02 -0500

    WEB-258: [bugfix] features, newUIDev

--- custom/_newUIDev/js/loadLibChat.js
+++
@@ -1,3 +0,0 @@
-var lcjs = document.createElement('script') ;
-lcjs.src = "https://v2.libanswers.com/load_chat.php?hash=7a4eb50897c2745e69d7fc4057f2b288";
-document.head.appendChild(lcjs);
--- custom/_newUIDev/js/loadjQuery.js
+++
@@ -1,5 +0,0 @@
-/* Load JQuery */
-
-var js = document.createElement('script') ;
-js.src = "//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js";
-document.head.appendChild(js);
